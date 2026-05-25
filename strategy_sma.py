d9dea9add8475ff746e9510ba80d2cad0327074f
DivitSharma008 <divits2008@gmail.com>
2026-05-25 14:31:22 +0530

    Update strategy_sma.py

--- strategy_sma.py
+++ strategy_sma.py
@@ -1,14 +1,16 @@
 import pandas as pd
 from datetime import date,timedelta
 def generate_signals(df):
+    df = df.copy()
     df["Date"] = pd.to_datetime(df["Date"])
     df = df.set_index("Date")
-    fiftydayMA = df["Close"].rolling(window="50D").mean()
-    twohundreddayMA = df["Close"].rolling(window="200D").mean()
-    signals = pd.Series(data=-1,index=df.index)
-    signals.loc[fiftydayMA>twohundreddayMA] = 1    
-    signals = signals.shift(1)
-    print(signals)
+    fiftydayMA = df["Close"].rolling(window=50).mean()
+    twohundreddayMA = df["Close"].rolling(window=200).mean()
+    signals = pd.Series(data=0,index=df.index)
+    signals.loc[fiftydayMA>twohundreddayMA] = 1
+    signals.loc[fiftydayMA<twohundreddayMA] = -1   
+    signals = signals.shift(1).fillna(0)
+    return signals
     # print(signals[signals == 1])
 reliance = pd.read_csv("C:\\Users\DELL\OneDrive\Desktop\\finpulse-lite\data\RELIANCE.csv")
-generate_signals(reliance)
+signals = generate_signals(reliance)
